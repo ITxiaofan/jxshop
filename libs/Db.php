1df9fdbed0f91a51905b181357918970dbbc2a7a
IT FAN <1724940950@qq.com>
2018-09-26 09:47:22 +0800

    DB类

--- libs/Db.php
+++ libs/Db.php
@@ -6,11 +6,19 @@ class Db
     private $pdo;
     private function __clone(){}
     private function __construct(){
-
+        $this->pdo = new \PDO('mysql:host=127.0.0.1;dbname=jxshop','root','123456');
+        $this->pdo->exec('SET NAMES utf8');
     }
     // 返回唯一对象
     public static function make(){
+        if(self::$_obj === null){
+            self::$_obj = new self;
 
+        }
+        return self::$_obj;
+    }
+    public function prepare($sql){
+        return $this->pdo->prepare($sql);
     }
 
 }
